37863ea91673b66f7c771f1c330cd0db1f7bf20f
Alexander Vázquez <elteoremadebeethoven@gmail.com>
2020-11-09 14:52:26 -0600

    Update sum_duration.sh

--- bash_scripts/sum_duration.sh
+++ bash_scripts/sum_duration.sh
@@ -1,1 +1,1 @@
-for i in *.mp4; do LC_ALL=C ffmpeg -i "$i" 2>&1 | awk -F: '/Duration:/{print $2*3600+$3*60+$4}'; done | paste -sd+ | bc | xargs -I TIME date -d@TIME -u +%H:%M:%S
+for i in $1.$2; do LC_ALL=C ffmpeg -i "$i" 2>&1 | awk -F: '/Duration:/{print $2*3600+$3*60+$4}'; done | paste -sd+ | bc | xargs -I TIME date -d@TIME -u +$d:%H:%M:%S
